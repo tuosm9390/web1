52b76739f46bb48d499c5914931331c9e62c18af
tuosm9390 <51076837+tuosm9390@users.noreply.github.com>
2020-05-10 18:24:28 +0900

    Add files via upload

---
+++ miniProject-master/src/com/kh/run/Run.java
@@ -0,0 +1,16 @@
+package com.kh.run;
+
+
+import com.kh.user.view.UserMenu;
+
+public class Run {
+	
+	public static void main(String[] args) {
+		
+		
+		UserMenu menu = new UserMenu();
+		menu.mainMenu();
+
+	}
+}
+                                
---
+++ miniProject-master/src/com/kh/server/run/ServerRun.java
@@ -0,0 +1,11 @@
+package com.kh.server.run;
+
+import com.kh.server.view.MultiServer;
+
+public class ServerRun {
+	public static synchronized void main(String[] args) {
+		MultiServer ms = new MultiServer();
+		ms.init();
+//		ms.start();
+	}
+}
